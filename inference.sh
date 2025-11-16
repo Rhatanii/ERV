44856d1494af6445bb38345216537ec1339c41bd
Rhatanii <ryool_1832@kaist.ac.kr>
2025-11-17 03:09:53 +0900

    Remove secondary eval files

--- inference.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-conf=/mnt/ssd_hs/Exp/R1-Omni/configs/inference-7B.yaml
-# /mnt/ssd_hs/Exp/R1-Omni/configs/inference_text_only.yaml
-
-CUDA_VISIBLE_DEVICES=0 HF_HUB_CACHE=/mnt/ssd_hs/.cache python inference.py --config ${conf}
